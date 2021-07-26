64796832fea6c9da9a785686079e7d5c6e185055
gomjellie <gomjellie@gmail.com>
2021-07-26 17:34:25 +0900

    fix: client yarn install 시 어마어마한 warning 제거

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -1,8 +1,8 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    test: ['./src/index.ts'],
+    test: ["./src/index.ts"],
   },
 
   module: {
@@ -10,34 +10,39 @@ module.exports = {
       {
         test: /\.ts?$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
         exclude: /node_modules/,
       },
       {
         test: /\.(css|s[ca]ss)$/,
         use: [
-          'style-loader',
-          'css-loader',
+          "style-loader",
+          "css-loader",
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              implementation: require('node-sass'),
+              implementation: require("node-sass"),
             },
           },
         ],
       },
       {
         test: /\.(woff|woff2|eot|ttf)$/,
-        loader: 'url-loader?limit=100000',
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          publicPath: "./dist/",
+          limit: 10000, // 10kb
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
-    path: path.resolve(__dirname, '../server/public'),
-    filename: 'build.js',
+    path: path.resolve(__dirname, "../server/public"),
+    filename: "build.js",
   },
 };
