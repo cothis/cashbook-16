755999cd00c4a92793b0211d7da69b89811ac3af
gomjellie <gomjellie@gmail.com>
2021-07-28 00:53:22 +0900

    chore: #App은 항상 존재한다고 가정

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,14 +1,12 @@
 import React from './core/react';
 import Counter from './component/Counter';
 
-const $app = document.querySelector('#App');
+const $app = <Element>document.querySelector('#App');
 
 const updateDOM = () => {
   window.requestAnimationFrame(() => {
-    if ($app) {
-      let App;
-      App = React.render(Counter, $app);
-    }
+    let App;
+    App = React.render(Counter, $app);
   });
 };
 
