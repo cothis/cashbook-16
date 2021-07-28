d93738821e5a05d105fc437df8cc583aead964fe
cothis <minho1030@naver.com>
2021-07-28 18:25:34 +0900

    setup: router 클래스 및 커스텀 엘리먼트 추가, 테스트코드 추가
    Co-authored-by: gomjellie <gomjellie@gmail.com>

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,14 @@
+import Router from '@/core/router';
 import MainView from '@/view/pages/main';
+import LoginPage from '@/view/pages/login';
+import RouterLink from '@/view/customElements/router-link';
+window.customElements.define('router-link', RouterLink);
 
 const $app = <HTMLElement>document.querySelector('#App');
+const router = new Router();
 
 const mainView = new MainView($app);
-mainView.render();
+const loginPage = new LoginPage($app);
+router.addRoutePath('main', mainView);
+router.addRoutePath('login', loginPage);
+router.route('login');
---
+++ client/src/view/pages/login.ts
@@ -0,0 +1,16 @@
+import Page from '@/view/pages/page';
+import html from '@/core/jsx';
+
+export default class LoginPage extends Page {
+  constructor(root: HTMLElement) {
+    super(root);
+  }
+
+  createDom(): HTMLElement {
+    return html`<div>
+      로그인 화면입니다.
+      <div>첫번째 블록입니다.</div>
+      <router-link to="main">메인으로</router-link>
+    </div>`;
+  }
+}
