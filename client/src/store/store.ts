bbd1b7c01d71e7d59a3647babcbdd51ca8868851
gomjellie <gomjellie@gmail.com>
2021-07-28 13:56:23 +0900

    setup: store 부모클래스 추가 및 utils.ts 생성
    Co-authored-by: cothis <minho1030@naver.com>

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,1 +1,26 @@
+import { cmpState } from '../utils';
 export interface BaseState {}
+
+export default class Store<S extends BaseState = BaseState> {
+  state: S;
+
+  constructor(initialState?: S) {
+    this.state = initialState ?? ({} as S);
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  /**
+   * @returns 변경 됐으면 true, 그대로면 false
+   */
+  setState(newState: Partial<S>) {
+    const nextState = { ...this.state, ...newState };
+    if (cmpState(this.state, nextState)) {
+      return false;
+    }
+    this.state = nextState;
+    return true;
+  }
+}
