03d6bb098b118b9ef124d891c0d7425d6a5e8de7
cothis <minho1030@naver.com>
2021-08-01 16:10:47 +0900

    feat: historyController 추가

---
+++ server/src/controllers/HistoryController.ts
@@ -0,0 +1,47 @@
+import { PaymentHistory } from '@/entity/paymentHistory.entity';
+import { Request, Response } from 'express';
+import historyService from '../services/HistoryService';
+
+interface historyQuery {
+  category?: number;
+  isIncome?: boolean;
+  startDate?: Date;
+  endDate?: Date;
+  method?: string;
+}
+
+class historyController {
+  constructor() {}
+
+  getHistories = (req: Request, res: Response) => {
+    const query: historyQuery = req.query;
+    const histories: PaymentHistory[] = historyService.getHistoires(query);
+
+    res.json(histories);
+  };
+
+  createHistory = (req: Request, res: Response) => {
+    const history: PaymentHistory = req.body;
+    const newHistory = historyService.createHistory(history);
+
+    res.json(newHistory);
+  };
+
+  updateHistory = (req: Request, res: Response) => {
+    const historyId = Number(req.params.historyId);
+    const history: PaymentHistory = req.body;
+    history.uuid = historyId;
+    const newHistory = historyService.updateHistory(history);
+
+    res.json(newHistory);
+  };
+
+  deleteHistory = (req: Request, res: Response) => {
+    const historyId = Number(req.params.historyId);
+    const deletedHistory = historyService.deleteHistory(history);
+
+    res.json(deletedHistory);
+  };
+}
+
+export default new historyController();
