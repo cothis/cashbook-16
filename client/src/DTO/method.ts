da3dc60e17dc5c97f5f3cff11792104f7e49031c
gomjellie <gomjellie@gmail.com>
2021-08-04 12:01:20 +0900

    chore: 저장이 안됐었네 ㅎㅎ

--- client/src/DTO/method.ts
+++ client/src/DTO/method.ts
@@ -1,30 +1,3 @@
 type methodType = '카드' | '현금' | '현대카드';
-type categoryType =
-  | '식비'
-  | '문화/여가'
-  | '미분류'
-  | '생활'
-  | '의료/건강'
-  | '교통';
 
-type getHistoryPiece = {
-  uuid: number;
-  githubId: string;
-  content: string;
-  amount: '8000';
-  isIncome: boolean;
-  payDate: Date;
-  methodId: number;
-  categoryName: categoryType;
-  method: {
-    id: number;
-    githubId: string;
-    name: methodType;
-  };
-  category: {
-    name: categoryType;
-    color: string; // '#332299'
-  };
-};
-
-export default methodType;
+export { methodType };
