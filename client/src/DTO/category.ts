a7ea1a692838e82f0078fc8576e292b7c8c8e4ad
gomjellie <gomjellie@gmail.com>
2021-08-04 12:00:42 +0900

    feat: GET /api/histories api 프론트 모듈 생성 및 서버에서 오는 데이터 타입들 정리

---
+++ client/src/DTO/category.ts
@@ -0,0 +1,9 @@
+type categoryType =
+  | '식비'
+  | '문화/여가'
+  | '미분류'
+  | '생활'
+  | '의료/건강'
+  | '교통';
+
+export { categoryType };
---
+++ client/src/DTO/history.ts
@@ -0,0 +1,24 @@
+import { categoryType } from './category';
+import { methodType } from './method';
+
+type getHistoryPiece = {
+  uuid: number;
+  githubId: string;
+  content: string;
+  amount: string; // int로 파싱해줘야함
+  isIncome: boolean;
+  payDate: Date;
+  methodId: number;
+  categoryName: categoryType;
+  method: {
+    id: number;
+    githubId: string;
+    name: methodType;
+  };
+  category: {
+    name: categoryType;
+    color: string; // '#332299'
+  };
+};
+
+export { getHistoryPiece };
